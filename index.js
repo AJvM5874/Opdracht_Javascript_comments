16fae6e9470b83d7aa28bdb55d03b5aaaa1adffb
AJvM5874 <67021280+AJvM5874@users.noreply.github.com>
2021-05-27 20:47:01 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log("Hello Winc Academy");
+// Dit is een grote som 
+/*console.log(1230941 * 1823794);
+console.log(637263 / 54); */
+// Nee je ziet geen comments een comment is er voor om je code overzichelijker te maken
+// je ziet uitkomst niet omdat comments niet worden getoont in de console
